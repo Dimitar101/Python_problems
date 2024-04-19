2db1f29fdf5bea77acff8b6e28add1b1ea0e5852
Dimitar K Dimitrov <150704248+Dimitar101@users.noreply.github.com>
2024-04-19 10:01:05 +0300

    Add files via upload

---
+++ 3. Leonardo DiCaprio Oscars.py
@@ -0,0 +1,11 @@
+Oscar_number = int(input())
+
+if Oscar_number == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif Oscar_number == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif Oscar_number != 88 and Oscar_number != 86 and Oscar_number < 88:
+    print("When will you give Leo an Oscar?")
+else:                                               #Oscar is over 88
+    print("Leo got one already!")
+    
